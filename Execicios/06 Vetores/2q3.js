a06e152fac4aec0f46dadb6fc899f48207fa457d
pl4cydo <placydo.doc@gmail.com>
2022-05-26 22:03:45 -0300

    mudei o codigo da 2q3.js para melhorar depois

--- Execicios/06 Vetores/2q3.js
+++ Execicios/06 Vetores/2q3.js
@@ -2,20 +2,13 @@
 // (pesquise a fórmula da multiplicação de vetores)
 
 var mankey = [2,5,4] 
-var abra = [3,6]
+var abra = [3,6,4]
 var mult = 0
 var soma = 0
 
-if (mankey.length > abra.length){
-    for (i=0; i<mankey.length; i++) {
-        mult = mankey[i] * abra[i]
+for (i=0; i<mankey.length; i++) {
+    mult = mankey[i] * abra[i]
         soma = soma + mult
-    }
-} else if (abra.length > mankey.length){
-    for (i=0; i<abra.length; i++) {
-        mult = mankey[i] * abra[i]
-        soma = soma + mult
-    }
 }
 
 
