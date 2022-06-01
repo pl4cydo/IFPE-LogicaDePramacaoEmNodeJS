52bf4861d77530ef779c70c5b760c067862f6aae
pl4cydo <placydo.doc@gmail.com>
2022-06-01 13:16:31 -0300

    aprendendo a usar break

--- Execicios/07 Arrays/1q7.js
+++ Execicios/07 Arrays/1q7.js
@@ -3,23 +3,25 @@
 function arcanine (array1, array2) {
     let boo = true;
     for (i=0; i<array1.length; i++) {
+        if (boo == false) break
         for (j=0; j<array1.length; j++) {
+            if (boo == false) break
             if (array1[i][j] != array2[i][j] && boo == true){
                 boo = false
-            }
+            } 
         }
     }
     return boo
 }
 
 let landorus1 = [
-    [10,20],
+    [1,20],
     [30,40]
 ]
 
 let landorus2 = [
     [10,20],
-    [3,40]
+    [30,40]
 ]
 
 console.log(arcanine(landorus1, landorus2))
