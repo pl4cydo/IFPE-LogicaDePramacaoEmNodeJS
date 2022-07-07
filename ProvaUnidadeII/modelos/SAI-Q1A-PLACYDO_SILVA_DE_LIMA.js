9004258031c9f0f82f0d03a9a587b24b5e807c58
pl4cydo <placydo.doc@gmail.com>
2022-07-07 16:38:03 -0300

    correçao Q1A

--- ProvaUnidadeII/modelos/SAI-Q1A-PLACYDO_SILVA_DE_LIMA.js
+++ ProvaUnidadeII/modelos/SAI-Q1A-PLACYDO_SILVA_DE_LIMA.js
@@ -18,7 +18,7 @@ let arcanine = [1,2,3,4,5,6,7,8,9,10];
 
 function zard(array) {
     for (i = 0; i < array.length; i++){
-        if (array[i] % 2 == 0) {
+        if (i % 2 == 0) {
           console.log(array[i]) 
         }
     }
